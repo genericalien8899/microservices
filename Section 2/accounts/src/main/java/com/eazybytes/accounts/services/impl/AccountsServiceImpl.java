3157420cb32d506c420555b7c871fe0c5a3e8f97
genericalien8899 <mailforgithubsync@gmail.com>
2026-01-29 22:23:50 +0530

    Account Service Impl changes done. Continue from 3:32 on video 19

--- Section 2/accounts/src/main/java/com/eazybytes/accounts/services/impl/AccountsServiceImpl.java
+++ Section 2/accounts/src/main/java/com/eazybytes/accounts/services/impl/AccountsServiceImpl.java
@@ -1,6 +1,10 @@
 package com.eazybytes.accounts.services.impl;
 
+import com.eazybytes.accounts.constants.AccountsConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import com.eazybytes.accounts.mapper.CustomerMapper;
 import com.eazybytes.accounts.repository.AccountRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.services.IAccountService;
@@ -16,6 +20,18 @@ public class AccountsServiceImpl implements IAccountService {
 
     @Override
     public void createAccount(CustomerDto customerDto) {
+        Customer customer = CustomerMapper.mapToCustomer(customerDto, new Customer());
+        Customer savedCustomer = customerRepository.save(customer);             // as id (primary key) will be generated storing it in new obj to see
 
+    }
+
+    public Accounts createNewAcconunt(Customer customer){
+        Accounts newAccount = new Accounts();
+        newAccount.setCustomerId(customer.getCustomerId());
+        long randomAccoountNumer = 1000000000L + new Random().nextInt(900000000);
+        newAccount.setAccountType(AccountsConstants.SAVINGS);
+        newAccount.setBranchAddress(AccountsConstants.ADDRESS);
+
+        return newAccount;
     }
 }
